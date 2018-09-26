5bfcd621c645d71bb9af9d46755d7c52b605a5f5
dymfyvanmeel <dymfyvanmeel1@gmail.com>
2018-09-26 13:06:42 +0200

    en nog een cirkel mensen

--- typography-001/src/main/kotlin/Typography001.kt
+++ typography-001/src/main/kotlin/Typography001.kt
@@ -17,6 +17,8 @@ class Typography001: Program() {
 
         drawFunc = {
             poster(drawer) {
+
+                drawer.fill = ColorRGBa.PINK
                 drawer.background(ColorRGBa.BLUE)
                 drawer.fontMap = FontImageMap.fromUrl("file:data/fonts/IBMPlexMono-Bold.ttf", 22.0, 2.0)
                 drawer.text("((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((", 0.0, 0.0)
